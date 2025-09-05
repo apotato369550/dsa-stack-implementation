3a6583f7633b77b51f38b73af8f9b4a3a7ed2af5
John Andre Yap <johnandresyap510@gmail.com>
2025-09-05 22:55:28 +0800

    Added stackcursor header and c file

---
+++ stackcursor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// make the rest of the functions here
+void push(Stack *S, char X);
+void pop(Stack *S);
+char top(Stack *S);
+bool isEmpty(Stack *S);
+bool isFull(Stack *S);
---
+++ stackcursor.h
@@ -0,0 +1,24 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+#define MAX 50
+
+typedef struct {
+    int link;
+    char data;
+} Node;
+
+typedef struct {
+    int Avail;
+    Node Elem[MAX];
+} Stack;
+
+
+void push(Stack *S, char X);
+void pop(Stack *S);
+char top(Stack *S);
+bool isEmpty(Stack *S);
+bool isFull(Stack *S);
+
+#endif
