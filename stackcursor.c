5d6e6e7fc9dda007231368016990087141e5fbfc
John Andre Yap <johnandresyap510@gmail.com>
2025-09-06 23:46:11 +0800

    Worked on init, alloc, and dealloc

--- stackcursor.c
+++ stackcursor.c
@@ -3,8 +3,43 @@
 #include "stackcursor.h"
 
 // make the rest of the functions here
-void push(Stack *S, char X);
-void pop(Stack *S);
-char top(Stack *S);
-bool isEmpty(Stack *S);
-bool isFull(Stack *S);
+void initVirtualHeap(Stack *S) {
+    int length = MAX - 1;
+    for (int ndx = 0; ndx < length; ndx++) {
+        S->Elem[ndx].link = ndx;
+    }   
+    S->Avail = MAX;
+}
+
+int allocSpace(Stack *S) {
+    int ptr = S->Avail;
+    if (ptr = -1) {
+        S->Avail = S->Elem[ptr].link;
+    }
+    return ptr;
+}
+
+int deallocSpace(Stack *S, int index) {
+    S->Elem[index].link = S->Avail;
+    S->Avail = index;
+}
+
+void push(Stack *S, char X) {
+
+}
+
+void pop(Stack *S) {
+
+}
+
+char top(Stack *S) {
+
+}
+
+bool isEmpty(Stack *S) {
+
+}
+
+bool isFull(Stack *S) {
+
+}
