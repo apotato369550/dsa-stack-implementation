bf660c75b6a396006875ad8ed3fa80985e39c255
OppenJayme <jaymekyerivan@gmail.com>
2025-09-08 08:13:33 +0800

    add: ADT array definitions

---
+++ stackarray.c
@@ -0,0 +1,27 @@
+#include "stackarray.h"
+
+void initStack(Stack *S){
+    S->top = -1;
+}
+void push(Stack *S, char X){
+    if (!isFull(S)){
+        S->data[++S->top] = X;
+    }
+}
+void pop(Stack *S){
+    if (!isEmpty(S)){
+        S->top--;
+    }
+}
+
+char top(Stack *S){
+    if (!isEmpty(S)){
+        return S->data[S->top];
+    }
+}
+bool isEmpty(Stack *S){
+    return (S->top == -1) ? true : false;
+}
+bool isFull(Stack *S){
+    return (S->top == MAX - 1) ? true : false;
+}
