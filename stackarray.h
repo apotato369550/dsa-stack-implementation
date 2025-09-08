613e122c8186fe2acfa54d5cdab769c6f3259d5c
OppenJayme <jaymekyerivan@gmail.com>
2025-09-08 08:09:29 +0800

    add: ADT STACK for arrays

---
+++ stackarray.h
@@ -0,0 +1,19 @@
+#ifndef STACKARRAY_H
+#define STACKARRAY_H
+
+#include <stdbool.h>
+#define MAX 50
+
+typedef struct {
+    char data[MAX];
+    int top;
+} Stack;
+
+void initStack(Stack *S);
+void push(Stack *S, char X);
+void pop(Stack *S);
+char top(Stack *S);
+bool isEmpty(Stack *S);
+bool isFull(Stack *S);
+
+#endif
