569067cd0523258d13607c26ff91fc3d5cc23023
John Andre Yap <johnandresyap510@gmail.com>
2025-09-08 09:27:42 +0800

    Added program description

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+/*****************************************************
+----- Group Members -----
+Kyer Ivan Jayme - Member 1
+Chelsea Rose Pimentel - Member 2
+John Andre Yap - Member 3 (+ cursor based)
+
+----- Program Description -----
+A program that implements a stack in the following implementations:
+ 1. ArrayList
+ 2. LinkedList
+ 3. Cursor-Based
+
+----- How To Run -----
+1. Copy files to local repository
+2. (using gcc) Run the command "gcc main.c <implementation_filename>.c"
+3. Run the .exe created: "./a.exe"
+4. Enjoy
+
+----- Date Finished -----
+September 8, 2025
+
+----- Submitted To -----
+Christine Pena
+
+*****************************************************/
+
 #include "stackArray.h"
 #include <stdio.h>
 
