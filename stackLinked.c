574e3281f62a81eb7ec8dca08f70d4ea8a36fe87
ChelcRose <143089725+ChelcRose@users.noreply.github.com>
2025-09-08 08:45:13 +0800

    updated

---
+++ stackLinked.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "stackLinked.h"
+
+/*
+void initStack(Stack *S) {
+    S->top = NULL;
+}
+
+//isFull() uhh
+bool isFull(Stack S){
+  //  (S.top != NULL)? true : false;
+}
+
+//isEmpty() uhh 
+bool isEmpty(Stack S){
+    ((S.top)!= -1) ? true: false;
+}
+
+//push()
+void push (Stack *S, char data) {
+    LIST temp = (LIST)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = S->top;
+    S->top = temp; 
+}
+
+//pop()
+void pop (Stack *S){
+    LIST temp = (LIST)malloc(sizeof(struct node));
+    temp = S->top;
+
+
+
+    free(temp);
+}
+
+//top() /
+int top(Stack S){
+    printf("%d", S.top->data);
+}
+
+
+//displayStack()
+//no traverse ang print
+
+*/
---
+++ stackLinked.h
@@ -0,0 +1,33 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX 10
+
+typedef struct node {
+    char data;
+    struct node *next;
+} *LIST;
+
+typedef struct {
+    LIST top;
+} Stack;
+
+void initStack(Stack*);
+
+bool isFull(Stack);
+
+bool isEmpty(Stack);
+
+void push (Stack*, char);
+
+void pop (Stack *S);
+
+int top(Stack S);
+
+void displayStack(Stack S);
+
+#endif
